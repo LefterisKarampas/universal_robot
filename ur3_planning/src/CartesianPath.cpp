eb5efa4c8634d32f41ff8356b710bcd6d16ee848
LefterisKarampas <sdi1400064@di.uoa.gr>
2017-08-22 16:09:31 +0300

    Goal & Cartesian Path Planning

--- ur3_planning/src/CartesianPath.cpp
+++ ur3_planning/src/CartesianPath.cpp
@@ -12,6 +12,7 @@
 #include <moveit/robot_state/robot_state.h>
 #include <moveit/robot_trajectory/robot_trajectory.h> 
 #include <moveit/trajectory_processing/iterative_time_parameterization.h>
+#include <moveit_msgs/ExecuteKnownTrajectory.h>
 
 
 using namespace std;
@@ -24,67 +25,60 @@ int main(int argc, char **argv) {
     ros::NodeHandle nodeHandle;
 
     moveit::planning_interface::MoveGroup group("manipulator");
+    moveit_msgs::ExecuteKnownTrajectory srv;
+    moveit::planning_interface::MoveGroup::Plan plan;
     group.setMaxVelocityScalingFactor(0.1);
     group.setMaxAccelerationScalingFactor(0.1);
-    group.setPlanningTime(10.0);
+    group.setPlanningTime(30.0);
     group.setNumPlanningAttempts(100);
     group.setPlannerId("RRTConnectkConfigDefault");
     group.setPoseReferenceFrame("world");
-    group.setGoalTolerance(0.1);
+    group.setGoalTolerance(0.01);
 
 
   
-  std::vector<geometry_msgs::Pose> waypoints;
-  geometry_msgs::Pose target_pose;
-  target_pose.position.x = atof(argv[1]);
-  target_pose.position.y = atof(argv[2]);
-  target_pose.position.z = atof(argv[3]);
-  waypoints.push_back(target_pose);
-  geometry_msgs::Pose target_pose1;
-  target_pose1.position.x = atof(argv[4]);
-  target_pose1.position.y = atof(argv[5]);
-  target_pose1.position.z = atof(argv[6]);
-  waypoints.push_back(target_pose1);
-  //waypoints.push_back(target_pose2);
-  moveit_msgs::RobotTrajectory trajectory_msg;
-  double fraction = group.computeCartesianPath(waypoints,
-                                               0.01,  // eef_step
-                                               0.0,   // jump_threshold
-                                               trajectory_msg, true);
-  // The trajectory needs to be modified so it will include velocities as well.
-  // First to create a RobotTrajectory object
-  robot_trajectory::RobotTrajectory rt(group.getCurrentState()->getRobotModel(), "manipulator");
+	std::vector<geometry_msgs::Pose> waypoints;
+	geometry_msgs::Pose target_pose;
+	for(int i=1;i<argc;i+=3){
+		geometry_msgs::Pose target_pose;
+		target_pose.position.x = atof(argv[i]);
+		target_pose.position.y = atof(argv[i+1]);
+		target_pose.position.z = atof(argv[i+2]);
+		waypoints.push_back(target_pose);
+	}
+	//waypoints.push_back(target_pose2);
+  	double fraction = group.computeCartesianPath(waypoints,
+                                                                                   0.001, // eef_step
+                                                                                   0.0, // jump_threshold
+                                                                                   srv.request.trajectory, true);
 
-  // Second get a RobotTrajectory from trajectory
-  rt.setRobotTrajectoryMsg(*group.getCurrentState(), trajectory_msg);
- 
-  // Thrid create a IterativeParabolicTimeParameterization object
-  trajectory_processing::IterativeParabolicTimeParameterization iptp;
-  bool success;
-  // Fourth compute computeTimeStamps
-  success = iptp.computeTimeStamps(rt);
-  ROS_INFO("Computed time stamp %s",success?"SUCCEDED":"FAILED");
+	robot_trajectory::RobotTrajectory rt (group.getCurrentState()->getRobotModel(), "manipulator");
+	rt.setRobotTrajectoryMsg(*group.getCurrentState(), srv.request.trajectory);
 
-  // Get RobotTrajectory_msg from RobotTrajectory
-  rt.getRobotTrajectoryMsg(trajectory_msg);
-  // Finally plan and execute the trajectory
-  moveit::planning_interface::MoveGroup::Plan plan;
-  plan.trajectory_ = trajectory_msg;
-  //group.setPoseTargets(waypoints);
-  //success = group.plan(plan);
-  ROS_INFO("Visualizing plan 4 (cartesian path) (%.2f%% acheived)",fraction * 100.0);  
-  ROS_INFO("plan: %s",success?"SUCCESS":"FAILED");
-  if(success) {
-    char q;
-    std::cout << "Please make sure that your robot can move freely between these poses before proceeding!\n";
-    std::cout << "Continue? y/n: ";
-    std::cin >> q;
-    std::cout << endl;
-    if(q == 'y'){
-       ROS_INFO("Moving...");
-        group.asyncExecute(plan);
-    }
-  sleep(5.0);
-  } 
+	// create a IterativeParabolicTimeParameterization object
+	trajectory_processing::IterativeParabolicTimeParameterization iptp;
+	bool success = iptp.computeTimeStamps(rt);
+	ROS_INFO("Computed time stamp %s",success?"SUCCEDED":"FAILED");
+
+	// Get RobotTrajectory_msg from RobotTrajectory
+	rt.getRobotTrajectoryMsg(srv.request.trajectory);
+	// Finally plan and execute the trajectory
+	plan.trajectory_ = srv.request.trajectory;
+	ROS_INFO("Visualizing plan (cartesian path) (%.2f%% acheived)",fraction * 100.0);
+
+	if (fraction >= 0.65 && success == true) {
+		char q;
+		std::cout << "Please make sure that your robot can move freely between these poses before proceeding!\n";
+        std::cout << "Continue? y/n: ";
+        std::cin >> q;
+        std::cout << std::endl;
+        if(q == 'y'){
+           ROS_INFO("Moving...");
+            group.execute(plan);
+        }
+	} else{
+		ROS_ERROR("FAIL planning!");
+	}
+
   return 0;
 }
--- ur3_planning/src/move_goal.cpp
+++ ur3_planning/src/move_goal.cpp
@@ -5,6 +5,8 @@
 #include <unistd.h>
 #include <moveit/move_group_interface/move_group.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit/planning_scene/planning_scene.h>
+#include <moveit/kinematic_constraints/utils.h>
 #include <moveit_msgs/DisplayRobotState.h>
 #include <moveit_msgs/DisplayTrajectory.h>
 #include <moveit_msgs/Constraints.h>
@@ -13,10 +15,16 @@
 
 using namespace std;
 
+/*bool userCallback(const robot_state::RobotState& kinematic_state, bool verbose)
+{
+  const double* joint_values = kinematic_state.getJointPositions("wrist_3_joint");
+  return (joint_values[0] > 0.0);
+}*/
+
 int main(int argc, char **argv) {
     //cout << argv[0] << endl;
-    if(argc != 4){
-        ROS_ERROR("FAILED waiting 3 positions for args");
+    if(argc <= 3){
+        ROS_ERROR("FAILED waiting 3+ positions for args");
         exit(1);
     }
     ros::init(argc, argv,"moveit_group_goal");
@@ -29,13 +37,18 @@ int main(int argc, char **argv) {
     group.setMaxAccelerationScalingFactor(0.1);
     group.setPlanningTime(10.0);
     group.setNumPlanningAttempts(100);
+    //group.allowReplanning(true);
     group.setPlannerId("RRTConnectkConfigDefault");
     group.setPoseReferenceFrame("world");
-	group.setGoalTolerance(0.01);
+	group.setGoalTolerance(0.001);
 
     group.setStartStateToCurrentState();
 
     robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
+    //robot_model::RobotModelPtr kinematic_model = robot_model_loader.getModel();
+    //planning_scene::PlanningScene planning_scene(kinematic_model);
+    //planning_scene.setStateFeasibilityPredicate(userCallback);
+
     robot_state::RobotState start_state(*group.getCurrentState());
     robot_model::RobotModelPtr robot_model_ptr = robot_model_loader.getModel();
     const robot_state::JointModelGroup* joint_model_group = robot_model_ptr->getJointModelGroup("manipulator");
@@ -48,22 +61,33 @@ int main(int argc, char **argv) {
     target_pose.pose.position.x = atof(argv[1]);
     target_pose.pose.position.y = atof(argv[2]);
     target_pose.pose.position.z = atof(argv[3]);
-    //target_pose.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(M_PI,0.0,0.0); //horizontal
-    target_pose.pose.orientation.w = 1.0;
+
+    //target_pose.pose.orientation.w = 1.0;
     geometry_msgs::Pose target_pose1;
     target_pose1.position.x = target_pose.pose.position.x;
     target_pose1.position.y = target_pose.pose.position.y;
     target_pose1.position.z = target_pose.pose.position.z;
-    target_pose1.orientation.w = 1.0;
-    std::cout << "BEFORE:" << target_pose1.position.x << std::endl;
+    if(argc > 4){
+        target_pose1.orientation = tf::createQuaternionMsgFromRollPitchYaw(atof(argv[4]),atof(argv[5]),atof(argv[6]));
+        target_pose.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(atof(argv[4]),atof(argv[5]),atof(argv[6])); //horizontal
+    }
+    else{
+        target_pose1.orientation = tf::createQuaternionMsgFromRollPitchYaw(M_PI,0.0,0.0);
+        target_pose.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(M_PI,0.0,0.0); //horizontal
+    }
     bool found_ik = start_state.setFromIK(joint_model_group, target_pose1);
     if(!found_ik){
         ROS_ERROR("Invalid end point!");
         return 2;
     }
-    std::cout << "AFTER!\n";
     moveit_msgs::Constraints constraint;
     //constraint 
+    
+    //bool state_feasible = planning_scene.isStateFeasible(start_state);
+    //ROS_INFO_STREAM("State is " << (state_feasible ? "feasible" : "not feasible"));
+
+    //bool state_valid = planning_scene.isStateValid(start_state, kinematic_constraint_set, "manipulator");
+    //ROS_INFO_STREAM("Test 10: Random state is " << (state_valid ? "valid" : "not valid"));
 
     group.setPoseTarget(target_pose);
     group.setPathConstraints(constraint);
@@ -79,7 +103,7 @@ int main(int argc, char **argv) {
         if(q == 'y'){
            ROS_INFO("Moving...");
             //group.stop();
-            group.execute(my_plan);
+            group.asyncExecute(my_plan);
         }
     } 
     sleep(5);
